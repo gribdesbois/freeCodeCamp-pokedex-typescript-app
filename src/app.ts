e8b6dacb5198a96cf0e2f7a1841a41764c761648
gribdesbois <gribdesbois@gmail.com>
2022-03-15 09:53:01 +0100

    getPokemon

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,32 @@
 const container: HTMLElement | any = document.getElementById('app')
 const pokemons = 100
+
+interface IPokemon {
+  id: number
+  name: number
+  image: string
+  type: string
+}
+
+const getPokemon = async (id: number): Promise<void> => {
+  const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const pokemon: any = await data.json()
+  const pokemonType: string = pokemon.types
+    .map((poke: any) => poke.type.name)
+    .join(', ')
+
+  const transformedPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    image: `${pokemon.sprites.front_default}`,
+    type: pokemonType,
+  }
+
+  showPokemon(transformedPokemon)
+}
+
+const fetchData = (): void => {
+  for (let i = 1; i <= pokemons; i++) {
+    getPokemon(i)
+  }
+}
